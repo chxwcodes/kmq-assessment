bed5862476f6816f55704ee9a66633c87156d26d
Chow <contact-chxw@gmail.com>
2022-10-07 10:24:18 -0400

    initialize react project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
 
 export default App;
+
+// req
+// landing pg of your own choice
+// create 2 tests w 10 dummy mcq w submit test
+// no need to display results or any calculations
+// code is modular and bug free
+// commit history
+// add desc & pics in readme
+
+// bonus
+// add database, calculate & show results pg
+
+// submit repo: https://coderbyte.com/question/test-taking-website-th-1qi5lphakw
+
+//due tues
